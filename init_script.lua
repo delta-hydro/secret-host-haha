9103cb6c9028c514ea4d1e4b2bbcea1d50af9dc0
Rexi <68111233+RexiRexii@users.noreply.github.com>
2023-10-31 15:51:28 +0300

    Update init_script.lua

--- init_script.lua
+++ init_script.lua
@@ -237,7 +237,7 @@ local function performRequest(options)
     return _coroutineyield();
 end;
 
-genv.request = newcclosure(function(options)
+genv.request = function(options)
     local headers = {
         ["User-Agent"] = userAgent
     };
@@ -259,7 +259,7 @@ genv.request = newcclosure(function(options)
     });
     res.Success = res.StatusCode >= 200 and res.StatusCode <= 299;
     return res;
-end);
+end;
 
 --[[ Input Library ]]--
 
