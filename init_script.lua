990520fd04c4baf2d02d9b2a0c1ef3e4986dbb10
Rexi <68111233+RexiRexii@users.noreply.github.com>
2023-12-09 21:44:12 +0300

    Delete init_script.lua

--- init_script.lua
+++
@@ -1,9 +0,0 @@
---[[ 
-Here's to the guy that refused to actually be helpful and steal money off of kids!
-
-You are a pooron and you serve 0 purpose in your life, all you could do was skid some vulns and steal some kids robux
-If you have the chance to do it; kill yourself
-
-surprise surprise, anyone thats been looking in here should now feel awful
-init script is actually hidden now, none of you stalkers shall access my init script now
---]]
