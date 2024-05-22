50516c839baf6ac885d239c79ae7063688d6eed5
lxnny <89148963+lxnnydev@users.noreply.github.com>
2024-05-23 00:51:17 +0200

    Update deltax_ui.lua

--- deltax_ui.lua
+++ deltax_ui.lua
@@ -5805,7 +5805,7 @@ function checkkey()
     local savedkey = readfile("dsigfiureikuger.txt")
 
 	local keyless = game:HttpGet("https://raw.githubusercontent.com/lxnnydev/DeltaAndroid/main/iskeyless")
-	if string.find(keyless, "false") then
+	if string.find(keyless, "true") then
 		GrantAccess()
         return true
 	end
